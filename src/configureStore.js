bee7bcbcbc3e98440b1a877f70ee4a72f0f2c21d
andgarzonmal <andgarzonmal@unal.edu.co>
2022-07-11 13:14:34 -0500

    import categories reducer

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { booksReducer } from './Redux/books/books';
+import { categoriesReducers } from './Redux/Categories/categories';
 
 const store = configureStore({
   reducer: {
     bookShelf: booksReducer,
+    categories: categoriesReducers,
   },
 });
 

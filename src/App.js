51e6ce28412c829702f6716ac5129ae20c7d78cc
andgarzonmal <andgarzonmal@unal.edu.co>
2022-07-11 13:09:28 -0500

    add provider

--- src/App.js
+++ src/App.js
@@ -1,35 +1,38 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Nav from './components/Nav';
 import Categories from './components/Categories';
 import Bookscontainer from './components/Bookscontainer';
 import Form from './components/Form';
+import store from './configureStore';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Nav />
-      <Routes>
-        <Route
-          path="/"
-          element={(
-            <div className="App">
-              <Bookscontainer />
-              <Form />
-            </div>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route
+            path="/"
+            element={(
+              <div className="App">
+                <Bookscontainer />
+                <Form />
+              </div>
+            )}
+          />
+          <Route
+            path="/Categories"
+            element={(
+              <div className="App">
+                <Categories />
+              </div>
           )}
-        />
-        <Route
-          path="/Categories"
-          element={(
-            <div className="App">
-              <Categories />
-            </div>
-        )}
-        />
-      </Routes>
-
-    </BrowserRouter>
+          />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 

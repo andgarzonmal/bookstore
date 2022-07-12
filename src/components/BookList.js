e71674767232cdf61b5c2323c8ac1d7c934e0bcb
andgarzonmal <andgarzonmal@unal.edu.co>
2022-07-12 12:51:31 -0500

    ➕ ADD logic

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,29 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const BookList = ({ title, author }) => (
-  <li>
-    <div>
-      <h1>{title}</h1>
-      <h2>{author}</h2>
-      <ul>
-        <li>
-          <button type="button">Comments</button>
-        </li>
-        <li>
-          <button type="button">Remove</button>
-        </li>
-        <li>
-          <button type="button">Edit</button>
-        </li>
-      </ul>
-    </div>
-  </li>
-);
+function BookList({
+  title, author, deletebook, id,
+}) {
+  return (
+    <li>
+      <div>
+        <h1>{title}</h1>
+        <h2>{author}</h2>
+        <ul>
+          <li>
+            <button type="button">Comments</button>
+          </li>
+          <li>
+            <button type="button" onClick={() => deletebook(id)}>Remove</button>
+          </li>
+          <li>
+            <button type="button">Edit</button>
+          </li>
+        </ul>
+      </div>
+    </li>
+  );
+}
 
 export default BookList;
 
 BookList.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
+  deletebook: PropTypes.func.isRequired,
+  id: PropTypes.number.isRequired,
 };

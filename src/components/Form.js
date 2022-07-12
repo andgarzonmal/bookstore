9e6f9be774ac09fa0fb3a7b091a284180996f432
andgarzonmal <andgarzonmal@unal.edu.co>
2022-07-12 12:53:04 -0500

    ADD form functionality

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux/es/exports';
+import { addbook } from '../Redux/books/books';
 
 export default function Form() {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const dispatch = useDispatch();
+
+  const handleTitle = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleAuthor = (e) => {
+    setAuthor(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    const mybook = {
+      id: Math.floor(Math.random() * 100),
+      title,
+      author,
+    };
+    dispatch(addbook(mybook));
+    setTitle('');
+    setAuthor('');
+  };
+
   return (
-    <div>
+    <form>
       <h1>ADD NEW BOOK</h1>
-      <input type="text" placeholder="book title" />
-      <input type="text" placeholder="book author" />
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => handleTitle(e)}
+        placeholder="book title"
+      />
+      <input
+        type="text"
+        value={author}
+        onChange={(e) => handleAuthor(e)}
+        placeholder="book author"
+      />
       <select name="cars" id="cars">
         <option value="Action">Action</option>
         <option value="History">History</option>
       </select>
-      <button type="button">ADD BOOK</button>
-    </div>
+      <button
+        type="button"
+        onClick={handleSubmit}
+      >
+        ADD BOOK
+      </button>
+    </form>
   );
 }
